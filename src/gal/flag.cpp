a6871ffc4a5e56b32271351d31571557d322eb14
Charles Rymal <charlesrymal@gmail.com>
2013-12-10 00:32:51 -0800

    conflict

---
+++ src/gal/flag.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include <gal/flag.h>
+
+gal::flag::flag():
+	flag_(0)
+{}
+void	gal::flag::set(unsigned int f)
+{
+	flag_ |= f;
+}
+void	gal::flag::unset(unsigned int f)
+{
+	flag_ &= !f;
+}
+void	gal::flag::toggle(unsigned int f)
+{
+	flag_ ^= f;
+}
+bool	gal::flag::all(unsigned int f)
+{
+	//printf("%i %i\n",flag_,f);
+	
+	return ( ( flag_ & f ) == f );
+}
+bool	gal::flag::any(unsigned int f)
+{
+	//printf("%i %i\n",flag_,f);
+	
+	return ( flag_ & f );
+}
+
+
---
+++ src/gal/flag.h
@@ -0,0 +1,20 @@
+#ifndef __GAL_FLAG_H__
+#define __GAL_FLAG_H__
+
+namespace gal
+{
+	class flag
+	{
+		public:
+			flag();
+			bool		any(unsigned int);
+			bool		all(unsigned int);
+			void		set(unsigned int);
+			void		unset(unsigned int);
+			void		toggle(unsigned int);
+			
+			unsigned int	flag_;
+	};
+}
+
+#endif
