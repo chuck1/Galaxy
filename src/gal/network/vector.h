b2768bdb83dbe5ad71604416daea02bb71ef1108
chuck1 <chuck1@users.noreply.github.com>
2014-01-08 10:11:53 -0800

    Update vector.h

--- src/gal/network/vector.h
+++ src/gal/network/vector.h
@@ -51,6 +51,49 @@ namespace gal
 				vec		vector_;
 				typename vec::size_type	size_;
 		};
+		template <class T> class vector_ext
+		{
+			public:
+				typedef std::shared_ptr<T> Ts;
+				typedef std::vector<Ts> vec;
+				
+
+				vector(): size_(0) {}
+				void	write(message_shared msg)
+				{
+					size_ = vector_.size();
+					
+					msg->write(&size_, sizeof(typename vec::size_type));
+					
+					Ts t;
+					for(auto it = vector_.begin(); it != vector_.end(); ++it)
+					{
+						t = *it;
+						assert(t);
+						
+						t->write(msg);
+					}
+				}
+				void	read(message_shared msg)
+				{
+					msg->read(&size_, sizeof(typename vec::size_type));
+
+					Ts t;
+					vector_.clear();
+					for(typename vec::size_type i = 0; i < size_; ++i)
+					{
+						t.reset(new T);
+						
+						t->read(msg);
+						
+						vector_.push_back(t);
+					}
+				}
+				
+				vec		vector_;
+				typename vec::size_type	size_;
+		};
+		
 	}
 }
 
