c63b56b5c62e91671f641455b85fae7cf99e7b2b
chuck1 <chuck1@users.noreply.github.com>
2014-01-12 16:42:01 -0800

    Update control.cpp

--- src/gal/control/control.cpp
+++ src/gal/control/control.cpp
@@ -11,76 +11,13 @@ gal::control::control::control():
 {
 
 }
-float gal::control::control::f(float nx, float ntarget, double time) {
-	x0 = x1;
-	x1 = nx;
-	
-	time0 = time1;
-	time1 = time;
-	
-	float e = ntarget - x1;
-	
-	// velocity
-	double dt = 0;
-	
-	if(t > 0)
-	{
-		dt = time1 - time0;
-		
-		v = (x1 - x0) / dt;
-	}
-	
-	// integral
-	i += e * dt;
-	
+float gal::control::control::f(float e, float v, double time) {
+
 	// force
-	float force = e * coeff_p + v * coeff_d + i * coeff_i;
+	float force = e1 * coeff_p + v * coeff_d;
 	
 	return force;
 }
-float gal::control::control::f(float e, double time) {
-
-	e0 = e1;
-	e1 = e;
-	
-	time0 = time1;
-	time1 = time;
-	
-	// velocity
-	double dt = 0;
-	
-	if(t > 0)
-	{
-		dt = time1 - time0;
-		
-		v = (e1 - e0) / dt;
-	}
-	
-	// integral
-	i += e1 * dt;
-	
-	// force
-	float force = e1 * coeff_p + v * coeff_d + i * coeff_i;
-	
-	return force;
-}
-int	t;
-
-float	x0;
-float	x1;
-float	v0;
-float	v1;
-float	a;
-
-float	p;
-float	d;
-float	i;
-
-
-
-
-
-
 
 
 
