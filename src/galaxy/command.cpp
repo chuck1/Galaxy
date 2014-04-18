2ecb77187862179fca97e6914951bc506f86b54b
Charles Rymal <charlesrymal@gmail.com>
2014-04-18 11:04:39 -0700

    cleanup/move maze

--- src/galaxy/command.cpp
+++ src/galaxy/command.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#include <command.hpp>
+#include <galaxy/command.hpp>
 
 using namespace std;
 
@@ -195,15 +195,13 @@ void	command::print_args()
 }
 void	command::print()
 {
-	printf("print %i %i\n", map_short_.size(), map_long_.size() );
+	printf("print %i %i\n", (int)map_short_.size(), (int)map_long_.size() );
 
-	for( auto it = map_short_.begin(); it !=  map_short_.end(); ++it )
-	{
+	for( auto it = map_short_.begin(); it !=  map_short_.end(); ++it ) {
 		print_short( *it );
 	}
 	
-	for( auto it = map_long_.begin(); it !=  map_long_.end(); ++it )
-	{
+	for( auto it = map_long_.begin(); it !=  map_long_.end(); ++it ) {
 		print_long( *it );
 	}
 
--- src/galaxy/free.cpp
+++ src/galaxy/free.cpp
@@ -1,23 +1,24 @@
-#include <gal/except.hpp>
-#include <gal/free.hpp>
+#include <galaxy/except.hpp>
+#include <galaxy/free.hpp>
 
 void		gal::assert(bool a)
 {
 	if (!a)
 	{
-		throw gal::except("assertion");
+		/** TODO remove gal::assert */
+		throw;// gal::except("assertion");
 	}
 }
 void		gal::assert(bool a, char const * c)
 {
-	char msg[256];
+	//char msg[256];
 	
 	if (!a)
 	{
-		strcat(msg, "assertion: \"");
+		/*strcat(msg, "assertion: \"");
 		strcat(msg, c);
-		strcat(msg, "\" failed");
-		throw gal::except(msg);
+		strcat(msg, "\" failed");*/
+		throw;// gal::except(msg);
 	}
 }
 
