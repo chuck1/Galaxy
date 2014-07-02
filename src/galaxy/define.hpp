c86a25bf9038e515d22a22553afe16acbe864ab8
Charles Rymal <charlesrymal@gmail.com>
2014-07-01 19:29:20 -0700

    clean out old files

--- src/galaxy/define.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __JESS_DEFINE_HPP__
-#define __JESS_DEFINE_HPP__
-
-#define JESS_FUNCSIG __PRETTY_FUNCTION__
-
-#endif
-
-
--- src/galaxy/timer/timer.cpp
+++
@@ -1,8 +0,0 @@
-#include <galaxy/timer/timer.hpp>
-
-gal::timer::timer::timer(double time):
-	time_(time)
-{
-	
-}
-
