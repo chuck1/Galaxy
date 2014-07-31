1ab5d33d19d5440f09a05d5de131072f7056bba6
Charles Rymal <charlesrymal@gmail.com>
2014-07-30 22:36:16 -0700

    update

--- FindGalaxy/GalaxyConfig.cmake
+++ FindGalaxy/GalaxyConfig.cmake
@@ -1,46 +1,9 @@
 
+FIND_PACKAGE(CMakeHelper)
 
 SET(components std log console network)
 SET(pkg_name Galaxy)
 
-
-STRING(TOLOWER ${pkg_name} pkg_name_lower)
-
-
-IF(${pkg_name_lower}_SHARED)
-	SET(shared_postfix "_shared")
-ENDIF()
-
-MESSAGE(STATUS "Find ${pkg_name}")
-
-FOREACH(c ${components})
-
-	LIST(FIND ${pkg_name}_FIND_COMPONENTS "${c}" idx_${c})
-	
-	#MESSAGE(STATUS "idx_${c}: ${idx_${c}}")
-	
-	
-	STRING(COMPARE EQUAL ${idx_${c}} "-1" not_${c})
-	
-	
-	IF(NOT ${not_${c}})
-		MESSAGE(STATUS "    ${c}")
-		FIND_PACKAGE(${pkg_name_lower}_${c}${shared_postfix})
-
-		SET(
-			${pkg_name}_LIBRARIES
-			${${pkg_name}_LIBRARIES}
-			${${pkg_name_lower}_${c}_LIBRARIES})
-		
-	ENDIF()
-	
-	#MESSAGE(STATUS "not_${c}: ${not_${c}}")
-	MESSAGE(STATUS "${pkg_name}_LIBRARIES ${${pkg_name}_LIBRARIES}")
-
-	
-ENDFOREACH()
-
-
-
+cmh_finder(${pkg_name} "${components}")
 
 
